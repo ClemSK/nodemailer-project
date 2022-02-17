f4ac4dda9c6bc228c3db1046c7569160c64a385b
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2022-02-16 23:21:44 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact', { layout: false });
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'Outlook365',
+    host: 'smtp.office365.com',
+    port: '587',
+    tls: {
+      ciphers: 'SSLv3',
+      rejectUnauthorized: false,
+    },
+    auth: {
+      user: process.env.NODEMAILER_EMAIL_1,
+      pass: process.env.PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Blue Chip Devs" <bluechipdevs@outlook.com>', // sender address
+    to: 'clemskyn@gmail.com', // list of receivers
+    subject: 'Node Contact Request', // Subject line
+    text: 'This is a new contact request?', // plain text body
+    html: output, // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', { layout: false, msg: 'Email has been sent' });
+  });
+});
+
+app.listen(3001, () => console.log('Server Started'));
